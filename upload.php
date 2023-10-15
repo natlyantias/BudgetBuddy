b80c5c18e6d18f3b88f328f7ff13acb994a84db4
Sean Rice <26256176+sean-rice17@users.noreply.github.com>
2023-10-15 15:58:02 -0400

    Create upload.html

---
+++ upload.php
@@ -0,0 +1,34 @@
+<?php
+$target_dir = "uploads/";
+$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+$uploadOk = 1;
+$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+
+// Check if file already exists
+if (file_exists($target_file)) {
+    echo "Sorry, file already exists.";
+    $uploadOk = 0;
+}
+
+// Check file size (adjust as needed)
+if ($_FILES["fileToUpload"]["size"] > 5000000) {
+    echo "Sorry, your file is too large.";
+    $uploadOk = 0;
+}
+
+// Allow certain file formats (you can customize this list)
+/* if($imageFileType != "txt" && $imageFileType != "pdf" && $imageFileType != "docx") {
+    echo "Sorry, only TXT, PDF, and DOCX files are allowed.";
+    $uploadOk = 0;
+} */
+
+if ($uploadOk == 0) {
+    echo "Sorry, your file was not uploaded.";
+} else {
+    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+        echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+    } else {
+        echo "Sorry, there was an error uploading your file.";
+    }
+}
+?>
