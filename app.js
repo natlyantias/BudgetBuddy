dfe03dd2c7e1ffbc8edb08a7fccb4b523fc6e6df
ic0nic01 <77649849+ic0nic01@users.noreply.github.com>
2023-10-19 13:29:33 -0400

    Changed a few pointers

--- app.js
+++ app.js
@@ -24,6 +24,44 @@ app.get('/aboutusindex.html', (req, res) => {
     res.sendFile('aboutusindex.html', { root: app.get('views') });
 });
 
+app.get('/index.html', (req, res) => {
+    res.sendFile('index.html', { root: app.get('views') });
+});
+
+app.get('/aboutusindex.html', (req, res) => {
+    res.sendFile('aboutusindex.html', { root: app.get('views') });
+});
+
+// Route for budgetsindex.html
+app.get('/budgetsindex.html', (req, res) => {
+    res.sendFile('budgetsindex.html', { root: app.get('views') });
+});
+
+// Route for createaccountindex.html
+app.get('/createaccountindex.html', (req, res) => {
+    res.sendFile('createaccountindex.html', { root: app.get('views') });
+});
+
+// Route for loginindex.html
+app.get('/loginindex.html', (req, res) => {
+    res.sendFile('loginindex.html', { root: app.get('views') });
+});
+
+// Route for reportindex.html
+app.get('/reportindex.html', (req, res) => {
+    res.sendFile('reportindex.html', { root: app.get('views') });
+});
+
+// Route for settingsindex.html
+app.get('/settingsindex.html', (req, res) => {
+    res.sendFile('settingsindex.html', { root: app.get('views') });
+});
+
+// Route for transactionindex.html
+app.get('/transactionindex.html', (req, res) => {
+    res.sendFile('transactionindex.html', { root: app.get('views') });
+});
+
 // error handling
 
 app.listen(port, (error) => {
