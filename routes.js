5a3fec9fcc5fb4cb4697a2a8c9c5d8fa132b3f5c
Sean Rice <26256176+sean-rice17@users.noreply.github.com>
2023-11-26 19:29:26 -0500

    update routes to .ejs

--- routes.js
+++ routes.js
@@ -14,39 +14,41 @@ module.exports = function (app) {
     // ---------- handle GETs
 
     app.get("/", function (req, res) {
-        res.sendFile("index.html", { root: page_dir });
+        res.render("index.ejs", { root: page_dir });
     });
 
-    app.get("/index.html", function (req, res) {
-        res.sendFile("index.html", { root: page_dir });
+    app.get("/index", function (req, res) {
+        res.render("index.ejs", { root: page_dir });
     });
 
-    app.get("/aboutusindex.html", (req, res) => {
-        res.sendFile("aboutusindex.html", { root: page_dir });
+    app.get("/aboutusindex", (req, res) => {
+        res.render("aboutusindex.ejs", { root: page_dir });
     });
 
-    app.get("/budgetsindex.html", (req, res) => {
-        res.sendFile("budgetsindex.html", { root: page_dir });
+    app.get("/budgetsindex", (req, res) => {
+        res.render("budgetsindex.ejs", { root: page_dir });
     });
 
-    app.get("/createaccountindex.html", (req, res) => {
-        res.sendFile("createaccountindex.html", { root: page_dir });
+    app.get("/createaccountindex", (req, res) => {
+        res.render("createaccountindex.ejs", { root: page_dir });
     });
 
-    app.get("/loginindex.html", (req, res) => {
-        res.sendFile("loginindex.html", { root: page_dir });
+    app.get("/loginindex", (req, res) => {
+        res.render("loginindex.ejs", { root: page_dir });
     });
 
-    app.get("/reportindex.html", (req, res) => {
-        res.sendFile("reportindex.html", { root: page_dir });
+    app.get("/reportindex", (req, res) => {
+        res.render("reportindex.ejs", { root: page_dir });
     });
 
-    app.get("/settings", (req, res) => {
-        res.sendFile("settingsindex.html", { root: page_dir });
+    app.get("/settingsindex", (req, res) => {
+        const plaidConn = false;
+
+        res.render("settingsindex.ejs", { plaidConn, root: page_dir });
     });
 
-    app.get("/transactionindex.html", (req, res) => {
-        res.sendFile("transactionindex.html", { root: page_dir });
+    app.get("/transactionindex", (req, res) => {
+        res.render("transactionindex.ejs", { root: page_dir });
     });
 
 
