e482fc5a75fbf3921fab36f06835e032d9d44d5e
Sean Rice <26256176+sean-rice17@users.noreply.github.com>
2023-12-04 12:04:46 -0500

    Update install_packages.bat

--- install_packages.bat
+++ install_packages.bat
@@ -12,7 +12,7 @@ cd /d "!script_dir!"
 
 :: Check if package-lock.json exists, and if so, use it for installation
 if exist package-lock.json (
-    npm ci || echo Failed to install packages.
+    npm i || echo Failed to install packages.
 ) else (
     npm install
 )
