8f7d2ae38a5e051bd037f06826e4b3bb77fabd79
ic0nic01 <77649849+ic0nic01@users.noreply.github.com>
2023-12-09 16:12:24 -0500

    Update routes.js

--- routes.js
+++ routes.js
@@ -208,9 +208,5 @@ router.get("/settings", checkLoggedIn, (req, res) => {
 
 });
 
-<<<<<<< Updated upstream
-
-=======
->>>>>>> Stashed changes
 // export all routes after they have been defined for use in app.js
 module.exports = router;
